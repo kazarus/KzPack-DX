abb9f07f13daec522bef5ce025e337b63a88595d
kazarus <kazarus@qq.com>
2017-01-19 14:55:50 +0800

    20170119

--- UniConfig/Class/Helpr_UniEngine.pas
+++ UniConfig/Class/Helpr_UniEngine.pas
@@ -61,7 +61,7 @@ begin
   try
     JSON:=TQJson.Create;
     JSON.FromRtti(Self);
-    JSON.SaveToFile(AFileName,teUTF8,True,doFormat);
+    JSON.SaveToFile(AFileName,teUTF8,False,doFormat);
   finally
     FreeAndNil(JSON);
   end;
@@ -74,7 +74,7 @@ begin
   try
     JSON:=TQJson.Create;
     JSON.FromRtti(AList);
-    JSON.SaveToFile(AFileName,teUTF8,True,doFormat);
+    JSON.SaveToFile(AFileName,teUTF8,False,doFormat);
   finally
     FreeAndNil(JSON);
   end;
