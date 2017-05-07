47d2e11652b7e4abd561690253fd442c248bc112
kazarus <kazarus@qq.com>
2017-05-07 14:17:46 +0800

    20170507

--- Class_AppCnfg.pas
+++ Class_AppCnfg.pas
@@ -12,12 +12,14 @@ type
     FSrvrMemo:string;
     FInUseZIP:Integer;
     FInUseTLS:Integer;
+    FUserLast:string;
   published
     property SrvrAddr:string  read FSrvrAddr write FSrvrAddr;
     property SrvrPort:string  read FSrvrPort write FSrvrPort;
     property SrvrMemo:string  read FSrvrMemo write FSrvrMemo;
     property InUseZIP:Integer read FInUseZIP write FInUseZIP;
     property InUseTLS:Integer read FInUseTLS write FInUseTLS;
+    property UserLast:string  read FUserLast write FUserLast;
   public
     constructor Create;
   end;
