fdc8bf0885621533a8ada10327afaab411956e7f
kazarus <kazarus@qq.com>
2017-05-11 13:28:10 +0800

    20170511

---
+++ XlsImport/Class/XlsImport.Class_Cell_Node.pas
@@ -0,0 +1,45 @@
+unit XlsImport.Class_Cell_Node;
+
+interface
+uses
+  SysUtils,Classes;
+
+type
+  TCellNode = class(TObject)
+  public
+    DataINDX:Integer;
+    ParentID:Integer;
+  public
+    Col: Integer;
+    Row: Integer;
+    Text: string;
+  public
+    Left: Integer;
+    Right: Integer;
+    Top: Integer;
+    Bottom: Integer;
+  public
+    class function  CopyIt(aCellNode:TCellNode):TCellNode;overload;
+    class procedure CopyIt(aCellNode:TCellNode;var Result:TCellNode);overload;
+  end;
+implementation
+
+
+class function TCellNode.CopyIt(aCellNode: TCellNode): TCellNode;
+begin
+  Result := TCellNode.Create;
+  TCellNode.CopyIt(aCellNode,Result);
+end;
+
+class procedure TCellNode.CopyIt(aCellNode: TCellNode; var Result: TCellNode);
+begin
+  Result.Col := aCellNode.Col;
+  Result.Row := aCellNode.Row;
+  Result.Text := aCellNode.Text;
+  Result.Left := aCellNode.Left;
+  Result.Right := aCellNode.Right;
+  Result.Top := aCellNode.Top;
+  Result.Bottom := aCellNode.Bottom;
+end;
+
+end.
---
+++ XlsImport/Class/XlsImport.NodeManager.pas
@@ -0,0 +1,180 @@
+unit XlsImport.NodeManager;
+
+interface
+uses
+  SysUtils, Classes, cxGraphics, cxControls, cxLookAndFeels,
+  cxLookAndFeelPainters, dxCore, dxCoreClasses, dxHashUtils, dxSpreadSheetCore,
+  dxSpreadSheetCoreHistory, dxSpreadSheetConditionalFormatting,
+  dxSpreadSheetConditionalFormattingRules, dxSpreadSheetClasses,
+  dxSpreadSheetContainers, dxSpreadSheetFormulas, dxSpreadSheetHyperlinks,
+  dxSpreadSheetFunctions, dxSpreadSheetGraphics, dxSpreadSheetPrinting,
+  dxSpreadSheetTypes, dxSpreadSheetUtils, dxSpreadSheet;
+
+type
+  TNodeManager = class(TObject)
+  public
+    class function ReadNode(aSpreadSheet:TdxSpreadSheet;var aList:TStringList):Boolean;
+  end;
+
+implementation
+
+uses
+  XlsImport.Class_Cell_Node,Class_KzDebug,Class_KzUtils;
+
+
+class function TNodeManager.ReadNode(aSpreadSheet: TdxSpreadSheet;
+  var aList: TStringList): Boolean;
+var
+  I,C,R:Integer;
+  ColEnded:Integer;
+  ColStart:Integer;
+  RowStart:Integer;
+  RowEnded:Integer;
+
+  cItem:TdxSpreadSheetMergedCell;
+  cCell:TdxSpreadSheetCell;
+
+  cIndx:Integer;
+  cList:TStringList;
+  cNode:TCellNode;
+  xNode:TCellNode;
+
+  function FindNext(aNode:TCellNode):Integer;
+  var
+    I:Integer;
+  begin
+    Result := 0;
+
+    for I := 0 to aList.Count-1  do
+    begin
+      xNode := TCellNode(aList.Objects[I]);
+      if xNode.DataINDX = aNode.DataINDX then Continue;                           //如果是自已,拉倒.
+      //@if (xNode.Left = xNode.Right) and (xNode.Top = xNode.Bottom) then  Continue;//如果非合并,拉倒.
+
+      if cNode.Top-1 = xNode.Bottom then
+      begin
+        if TKzUtils.NumbInRect(cNode.Left,xNode.Left,xNode.Right) then
+        begin
+          if TKzUtils.NumbInRect(cNode.Right,xNode.Left,xNode.Right) then
+          begin
+            Result := xNode.DataINDX;
+            Break;
+          end;
+        end;
+      end;
+    end;
+  end;
+begin
+  Result := False;
+
+  if aList = nil then Exit;
+  TKzUtils.JustCleanList(aList);
+
+  with aSpreadSheet.ActiveSheetAsTable do
+  begin
+    ColStart := Selection.Area.Left;
+    ColEnded := Selection.Area.Right;
+    RowStart := Selection.Area.Top;
+    RowEnded := Selection.Area.Bottom;
+
+    for C := ColStart to ColEnded do
+    begin
+      for R := RowStart to RowEnded do
+      begin
+        cCell := Cells[R,C];
+        if cCell <> nil then
+        begin
+          if Trim(cCell.DisplayText)='' then Continue;
+
+          cNode := TCellNode.Create;
+          cNode.Col := C;
+          cNode.Row := R;
+          cNode.Left := C;
+          cNode.Top := R;
+          cNode.Right := C;
+          cNode.Bottom := R;
+
+          cNode.Text := Trim(cCell.DisplayText);
+
+          with cNode do
+          begin
+            aList.AddObject(Format('%D-%D',[cNode.Col,cNode.Row]),cNode);
+          end;
+        end;
+      end;
+    end;
+
+    for I := 0 to MergedCells.Count-1 do
+    begin
+      cItem:=MergedCells.Items[I];
+      if cItem=nil then Continue;
+
+      //#if not Selection.HasArea(cItem.Area) then Continue;
+
+      //#KzDebug.FileFmt('%S',[Trim(cItem.ActiveCell.DisplayText)]);
+      with cItem.Area do
+      begin
+        //#KzDebug.FileFmt('%S:%D:%D:%D:%D:%S',[self.ClassName,LEFT,RIGHT,TOP,BOTTOM,Trim(cItem.ActiveCell.DisplayText)]);
+
+        cNode := TCellNode.Create;
+        cNode.Col := cItem.Area.Left;
+        cNode.Row := cItem.Area.Top;
+        cNode.Text := Trim(cItem.ActiveCell.DisplayText);
+        cNode.Left := cItem.Area.Left;
+        cNode.Right := cItem.Area.Right;
+        cNode.Top := cItem.Area.Top;
+        cNode.Bottom := cItem.Area.Bottom;
+      end;
+
+      cIndx := aList.IndexOf(Format('%D-%D',[cNode.Col,cNode.Row]));
+      if cIndx = -1 then
+      begin
+        //#xNode := TCellNode.Create;
+        //#TCellNode.CopyIt(cNode,xNode);
+
+        //#aList.AddObject(Format('%D-%D',[xNode.Col,xNode.Row]),xNode);
+      end else
+      begin
+        xNode := TCellNode(aList.Objects[cIndx]);
+        if xNode <> nil then
+        begin
+          TCellNode.CopyIt(cNode,xNode);
+        end;
+      end;
+    end;
+  end;
+
+  for I := 0 to aList.Count-1 do
+  begin
+    cNode := TCellNode(aList.Objects[I]);
+    if cNode = nil then Continue;
+    cNode.DataINDX := I+1;
+
+    //#KzDebug.FileFmt('%S:%D:%D:%D:%D:%D:%D:%S',[self.ClassName,cNode.Col,cNode.Row,cNode.Left,cNode.Right,cNode.Top,cNode.Bottom,cNode.Text]);
+  end;
+
+  for I := 0 to aList.Count-1 do
+  begin
+    cNode := TCellNode(aList.Objects[I]);
+    if cNode = nil then Continue;
+    cNode.DataINDX := I+1;
+    cNode.ParentID := FindNext(cNode);
+
+    //#KzDebug.FileFmt('%S:%D:%D:%D:%D:%D:%D:%S',[self.ClassName,cNode.Col,cNode.Row,cNode.Left,cNode.Right,cNode.Top,cNode.Bottom,cNode.Text]);
+  end;
+
+  for I := 0 to aList.Count-1 do
+  begin
+    cNode := TCellNode(aList.Objects[I]);
+    if cNode = nil then Continue;
+
+    KzDebug.FileFmt('%S:%D:%D:%D:%D:%D:%D:%S_DATAINDX:%D_PARENTID:%D',[self.ClassName,cNode.Col,cNode.Row,cNode.Left,cNode.Right,cNode.Top,cNode.Bottom,cNode.Text,cNode.DataINDX,cNode.ParentID]);
+  end;
+
+
+
+
+  Result := True;
+end;
+
+end.
