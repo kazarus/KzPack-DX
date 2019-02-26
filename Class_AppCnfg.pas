a76644eb000332636ea2aaa3b1f1f8be850e23b6
kazarus <kazarus@qq.com>
2019-02-26 16:38:40 +0800

    JZPZ_20190226

--- Class_AppCnfg.pas
+++ Class_AppCnfg.pas
@@ -7,19 +7,19 @@ uses
 type
   TAppCnfg=class(TUniEngine)
   private
-    FSrvrAddr:string;
-    FSrvrPort:string;
-    FSrvrMemo:string;
-    FInUseZIP:Integer;
-    FInUseTLS:Integer;
-    FUserLast:string;
+    FSrvrAddr: string;
+    FSrvrPort: string;
+    FSrvrMemo: string;
+    FInUseZIP: Integer;
+    FInUseTLS: Integer;
+    FUserLast: string;
   published
-    property SrvrAddr:string  read FSrvrAddr write FSrvrAddr;
-    property SrvrPort:string  read FSrvrPort write FSrvrPort;
-    property SrvrMemo:string  read FSrvrMemo write FSrvrMemo;
-    property InUseZIP:Integer read FInUseZIP write FInUseZIP;
-    property InUseTLS:Integer read FInUseTLS write FInUseTLS;
-    property UserLast:string  read FUserLast write FUserLast;
+    property SrvrAddr: string read FSrvrAddr write FSrvrAddr;
+    property SrvrPort: string read FSrvrPort write FSrvrPort;
+    property SrvrMemo: string read FSrvrMemo write FSrvrMemo;
+    property InUseZIP: Integer read FInUseZIP write FInUseZIP;
+    property InUseTLS: Integer read FInUseTLS write FInUseTLS;
+    property UserLast: string read FUserLast write FUserLast;
   public
     constructor Create;
   end;
